0adb0f0bb347254e0f8b4768a640601581778a4a
Yen Lin <eric2chen@hannstar.com>
2026-04-29 13:33:48 +0800

    新增 AppFeature, AppFeatureTests

--- Features/Sources/Features/SpotifyCloneAppV2.swift
+++ Features/Sources/Features/SpotifyCloneAppV2.swift
@@ -5,6 +5,7 @@
 //  Created by Yen Lin on 2026/4/29.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
@@ -14,4 +15,12 @@ struct SpotifyCloneAppV2: App {
             AppInitialView()
         }
     }
+}
+
+struct AppFeature: Reducer {
+    struct State: Equatable { }
+    
+    enum Action: Equatable { }
+    
+    func reduce(into state: inout State, action: Action) -> Effect<Action> { }
 }
