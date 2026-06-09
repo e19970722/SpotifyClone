b03a759d465dcc32ab20fef207360e87e04504b9
Yen Lin <e19970722@gmail.com>
2026-06-09 11:39:51 +0800

    設定背景可繼續播放

--- SpotifyClone/SpotifyCloneApp.swift
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 @main
 struct SpotifyCloneApp: App {
+    
+    init() {
+        configureAudioSession()
+    }
+    
     var body: some Scene {
         WindowGroup {
             AppInitialView()
+        }
+    }
+}
+
+extension SpotifyCloneApp {
+    func configureAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(
+                .playback,
+                mode: .moviePlayback,
+                options: []
+            )
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+        } catch {
+            print("❌ AVAudioSession setup failed: \(error)")
         }
     }
 }
